97a26a8c311acc2330ad2a481dbc770190a4d8de
WdataW <wael272726@gmail.com>
2025-11-28 01:54:00 +0300

    feat: Added digital font to the calculator screen

--- src/ui/components/display/Screen.tsx
+++ src/ui/components/display/Screen.tsx
@@ -5,8 +5,10 @@ type Props = {
 export default function Screen({ className = '', expression, ...props }: Props) {
     return (
 
-        <div className={`bg-[var(--color-screen-bg)] w-full px-[1rem] rounded-t-[1.4rem] overflow-hidden    py-[0.4rem] text-[1.5rem] text-white h-[6rem]  ${className}`} {...props}>
-            {expression}
+        <div className={` text-nowrap relative bg-[var(--color-screen-bg)] font-[DIG] w-full px-[1rem] rounded-t-[1.4rem] overflow-hidden    py-[0.4rem] text-[1.5rem] text-white h-[6rem]  ${className}`} {...props}>
+            <p className="absolute blur-[0.15rem] opacity-50">{expression}</p>
+            <p >{expression}</p>
+
         </div>
     );
 }
