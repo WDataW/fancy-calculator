049954dc4444b856825ecd9e43c0775d95a1e9ae
WdataW <wael272726@gmail.com>
2026-01-08 01:35:20 +0300

    feat: Added design credits
    the calculator design is inspired by one of @dagnt2l 's works on x.com

--- src/ui/Calculator.tsx
+++ src/ui/Calculator.tsx
@@ -18,9 +18,12 @@ export default function Calculator({ className = '', ...props }: Props) {
     }, [expression]);
 
     return (
-        <div className={`${className} bg-linear-to-br from-[var(--color-calc-gradient-secondary)]  to-40% to-[var(--color-calc-gradient-primary)] p-[0.1rem]  rounded-[1.5rem] w-[calc(100%-2rem)] max-w-[19rem] `} {...props}>
-            <Screen result={result} blinker={blinker} setBlinker={setBlinker} setExpression={setExpression} expression={expression}></Screen>
-            <Keypad className="mt-[0.1rem]" blinker={blinker} setBlinker={setBlinker} setResult={setResult} expression={expression} setExpression={setExpression}></Keypad>
+        <div className={`${className} w-[calc(100%-2rem)] flex flex-col justify-center max-w-[19rem]`}>
+            <div className={` bg-linear-to-br from-[var(--color-calc-gradient-secondary)]  to-40% to-[var(--color-calc-gradient-primary)] p-[0.1rem] w-full  rounded-[1.5rem]  `} {...props}>
+                <Screen result={result} blinker={blinker} setBlinker={setBlinker} setExpression={setExpression} expression={expression}></Screen>
+                <Keypad className="mt-[0.1rem]" blinker={blinker} setBlinker={setBlinker} setResult={setResult} expression={expression} setExpression={setExpression}></Keypad>
+            </div>
+            <p className="ps-[0.2rem] text-white ">Design inspired by <a target="_blank" className="text-blue-400" href="https://x.com/dagnt2l">@dagnt2l</a></p>
         </div>
     );
 }
